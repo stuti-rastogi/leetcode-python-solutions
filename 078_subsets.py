4ea7500f0ba47d6f7a6010ecdfc9701080a00339
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2020-06-01 19:51:24 -0400

    Some formatting changes

--- 078_subsets.py
+++ 078_subsets.py
@@ -1,22 +1,22 @@
-class Solution:
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        # using python itertools
-        # result = []
-        # for i in range(0, len(nums)+1):
-        #     result = result + list(itertools.combinations(nums, i))
-        # return result
+# class Solution:
+#     def subsets(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[List[int]]
+#         """
+#         # using python itertools
+#         # result = []
+#         # for i in range(0, len(nums)+1):
+#         #     result = result + list(itertools.combinations(nums, i))
+#         # return result
     
-        result = [[]]
-        for i in nums:
-            current = []
-            for subset in result:
-                current.append(subset + [i])
-            result = result + current
-        return result
+#         result = [[]]
+#         for i in nums:
+#             current = []
+#             for subset in result:
+#                 current.append(subset + [i])
+#             result = result + current
+#         return result
 
 class Solution(object):
     def subsets(self, nums):
