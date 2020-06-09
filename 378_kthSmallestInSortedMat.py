7bce6ad1f5a0ef3c22a5f130cc65c8b1dcf42fe1
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2020-06-09 19:37:27 -0400

    Changes for 378

--- 378_kthSmallestInSortedMat.py
+++ 378_kthSmallestInSortedMat.py
@@ -8,3 +8,25 @@ class Solution:
         return sorted([element for row in matrix for element in row])[k-1]
 
         # sort - O(nlogn)
+
+    # from heapq import *
+
+    # def find_Kth_smallest(matrix, k):
+    #     minHeap = []
+
+    #     # put the 1st element of each row in the min heap
+    #     # we don't need to push more than 'k' elements in the heap
+    #     for i in range(min(k, len(matrix))):
+    #         heappush(minHeap, (matrix[i][0], 0, matrix[i]))
+
+    #     # take the smallest(top) element form the min heap, if the running count is equal to k' return the number
+    #     # if the row of the top element has more elements, add the next element to the heap
+    #     numberCount, number = 0, 0
+    #     while minHeap:
+    #         number, i, row = heappop(minHeap)
+    #         numberCount += 1
+    #         if numberCount == k:
+    #             break
+    #         if len(row) > i+1:
+    #             heappush(minHeap, (row[i+1], i+1, row))
+    #     return number
