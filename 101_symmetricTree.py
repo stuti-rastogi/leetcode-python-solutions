b13e0baf39a6d46e0cb58b0d18c40dd4aa726cc5
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2020-06-09 20:23:05 -0400

    Reformatting readme, adding solution for 200

--- 101_symmetricTree.py
+++ 101_symmetricTree.py
@@ -12,15 +12,15 @@ class Solution:
         :rtype: bool
         """
         return self.checkMirror(root, root)
-        
+
     def checkMirror(self, tree1, tree2):
         if (not tree1 and not tree2):
             return True
-        
+
         if (not tree1 or not tree2):
             return False
-        
+
         if (tree1 and tree2):
-            return ((tree1.val == tree2.val) and self.checkMirror(tree1.left, tree2.right) and self.checkMirror(tree1.right,                        tree2.left))
-        
+            return ((tree1.val == tree2.val) and self.checkMirror(tree1.left, tree2.right) and self.checkMirror(tree1.right, tree2.left))
+
         return False
