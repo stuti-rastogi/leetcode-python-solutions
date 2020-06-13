b2f540e571d7d331214705d1464795a7c94c081c
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2020-06-12 21:36:27 -0400

    Simplified solution for 300

--- 300_LIS.py
+++ 300_LIS.py
@@ -7,14 +7,14 @@
         # O(n^2) complexity
 #         if not nums:
 #             return 0
-        
+
 #         n = len(nums)
 #         aux = [1] * n
 #         for i in range(n):
 #             for j in range(i):
 #                 if (nums[j] < nums[i] and aux[i] < aux[j] + 1):
 #                     aux[i] = aux[j] + 1
-        
+
 #         return max(aux)
 
 class Solution:
@@ -22,39 +22,38 @@ class Solution:
         '''
         O(nlogn) Binary Search+Greedy
         '''
+        def binarySearch(arr, target):
+            '''
+            find the position of this num
+            '''
+            low = 0
+            high = len(arr)
+
+            while (low < high):
+                mid = (low + high) // 2
+                if arr[mid] == target:
+                    return mid
+                if arr[mid] < target:
+                    low = mid+1
+                else:
+                    high = mid
+
+            if low == len(arr):
+                return low-1
+            return low
+
+
         if not nums:
             return 0
-        self.tail_number = [nums[0]]
-        
-        for i in range(1, len(nums)):  # N
-            # case 3, greater than all end elements
-            if nums[i] > self.tail_number[-1]:
-                self.tail_number.append(nums[i])
-                #print(self.tail_number)
-            else:                                  # logN
-                # case 2: where does this number lie, update the end element there
-                pos = self.binarySearch(nums[i])
-                self.tail_number[pos] = nums[i]
-        
-        return len(self.tail_number)
 
-    def binarySearch(self, num):
-        '''
-        find the first tail larger than num
-        '''
-        left = 0
-        right = len(self.tail_number) - 1
-        while left + 1 < right:
-            mid = left + (right - left) // 2
-            if self.tail_number[mid] > num:
-                    right = mid
-            elif self.tail_number[mid] < num:
-                    left = mid
-            if self.tail_number[mid] == num:
-                    right = mid
-                    
-        if self.tail_number[left] >= num:
-            return left
-        elif self.tail_number[right] >= num:
-            return right
-        
+        sequence = [nums[0]]
+
+        for num in nums:
+            if num < sequence[0]:
+                sequence[0] = num
+            elif num > sequence[-1]:
+                sequence.append(num)
+            else:
+                sequence[binarySearch(sequence, num)] = num
+
+        return len(sequence)
