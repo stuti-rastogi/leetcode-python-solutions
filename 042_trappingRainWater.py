e227ff9e3a56b2f4b321763ce01e47b8302e4c2c
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2020-09-07 19:27:14 -0400

    Adding stack soln for 042

--- 042_trappingRainWater.py
+++ 042_trappingRainWater.py
@@ -22,25 +22,45 @@ class Solution(object):
         # return total
     
         # 2 pointers - O(n)
-        n = len(height)
-        total = 0
-        left = 0
-        right = n-1
+        # n = len(height)
+        # total = 0
+        # left = 0
+        # right = n-1
         
-        maxLeftHeight = 0
-        maxRightHeight = 0
+        # maxLeftHeight = 0
+        # maxRightHeight = 0
         
-        while (left < right):
-            if (height[left] < height[right]):
-                if (height[left] >= maxLeftHeight):
-                    maxLeftHeight = height[left]
-                else:
-                    total = total + (maxLeftHeight - height[left])
-                left = left + 1
-            else:
-                if (height[right] >= maxRightHeight):
-                    maxRightHeight = height[right]
-                else:
-                    total = total + (maxRightHeight - height[right])
-                right = right - 1
-        return total
+        # while (left < right):
+        #     if (height[left] < height[right]):
+        #         if (height[left] >= maxLeftHeight):
+        #             maxLeftHeight = height[left]
+        #         else:
+        #             total = total + (maxLeftHeight - height[left])
+        #         left = left + 1
+        #     else:
+        #         if (height[right] >= maxRightHeight):
+        #             maxRightHeight = height[right]
+        #         else:
+        #             total = total + (maxRightHeight - height[right])
+        #         right = right - 1
+        # return total
+
+        # Stack solution
+        class Solution:
+            def trap(self, height):
+                stack = []
+                result = 0
+                
+                for i, v in enumerate(height):
+                    while len(stack) > 0 and height[stack[-1]] < v:
+                        lowHeight = height[stack.pop()]
+                        if len(stack) == 0:
+                            break
+                        leftBoundInd = stack[-1]
+                        heightDiff = min(height[leftBoundInd], v) - lowHeight
+                        width = i - leftBoundInd - 1
+                        result += heightDiff * width
+                    stack.append(i)
+
+                return result
+                
