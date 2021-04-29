6f27ad11d39e71f02c50985a0f97f8e3d76c64b7
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2021-04-29 19:56:54 +0530

    Adding new method of solving 230

--- 230_kthSmallestInBST.py
+++ 230_kthSmallestInBST.py
@@ -5,34 +5,34 @@
 #         self.left = None
 #         self.right = None
 
-# class Solution:
+class Solution:
 
-#     ################################
-#     ########### METHOD 1 ###########
-#     ################################
+    ################################
+    ########### METHOD 1 ###########
+    ################################
 
-#     def countNodes(self, root):
-#         if not root:
-#             return 0
-#         return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+    def countNodes(self, root):
+        if not root:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
 
-#     def kthSmallest(self, root, k):
-#         """
-#         :type root: TreeNode
-#         :type k: int
-#         :rtype: int
-#         """
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
 
-#         if not root:
-#             return
+        if not root:
+            return
         
-#         count = self.countNodes(root.left)
-#         if (k <= count):
-#             return self.kthSmallest(root.left, k)
-#         elif (k > count+1):
-#             return self.kthSmallest(root.right, k - count - 1)
-#         else:
-#             return root.val
+        count = self.countNodes(root.left)
+        if (k <= count):
+            return self.kthSmallest(root.left, k)
+        elif (k > count+1):
+            return self.kthSmallest(root.right, k - count - 1)
+        else:
+            return root.val
 
     ################################
     ########### METHOD 2 ###########
