c91a6100389861019954074511a7e1e14a1b8da3
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2021-05-18 17:21:15 +0530

    054 spiral matrix modifications

--- 054_spiralMatrix.py
+++ 054_spiralMatrix.py
@@ -4,41 +4,38 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-    
-        result = []
-        if not matrix:
-            return result
-        
         m = len(matrix)
         n = len(matrix[0])
-        
+
+        result = []
+        aimNumber = m * n
+
         left = 0
         right = n-1
         top = 0
         bottom = m-1
-        
-        while (len(result) < m*n):
-            i = top
-            for j in range(left, right+1):
-                result.append(matrix[i][j])
-            top = top+1
-            if (len(result) == m*n):
-                break
-            for i in range(top, bottom+1):
-                result.append(matrix[i][j])
-            right = right-1
-            if (len(result) == m*n):
-                break
-            for j in range(right,left-1,-1):
-                result.append(matrix[i][j])
-            bottom = bottom-1
-            if (len(result) == m*n):
-                break
-            for i in range(bottom,top-1,-1):
-                result.append(matrix[i][j])
-            left = left+1
-            if (len(result) == m*n):
-                break
-        
-        return result
-                
+
+        while True:
+            for col in range(left, right+1):
+                result.append(matrix[top][col])
+            top = top + 1
+            if len(result) == aimNumber:
+                return result
+
+            for row in range(top, bottom+1):
+                result.append(matrix[row][right])
+            right = right - 1
+            if len(result) == aimNumber:
+                return result
+
+            for col in range(right, left-1, -1):
+                result.append(matrix[bottom][col])
+            bottom = bottom - 1
+            if len(result) == aimNumber:
+                return result
+
+            for row in range(bottom, top-1, -1):
+                result.append(matrix[row][left])
+            left = left + 1
+            if len(result) == aimNumber:
+                return result
