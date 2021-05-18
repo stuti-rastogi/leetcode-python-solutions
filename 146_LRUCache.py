6e7a558c69ba60d38f73287669a040e4d67bc376
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2021-05-18 21:05:05 +0530

    cleaned up 146 lru cache

--- 146_LRUCache.py
+++ 146_LRUCache.py
@@ -1,99 +1,84 @@
-#define double linked list node (DLL) and used as cache
-class Node(object):
-    def __init__(self,key,value):
-        self.key=key
-        self.value=value
-        self.next=None
-        self.prev=None
-        
-class LRUCache(object):
-    #define <key:node> hashtable, head node and tail node
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
-        self.capacity=capacity
-        self.hashTable={}
-        self.hd=None
-        self.tl=None
-        
-    #remove node from DLL
-    def removeNode(self,node):
-        #if the removed node is head, remove head
-        if node==self.hd:
-            self.hd=self.hd.next
-            if self.hd:
-                self.hd.prev=None
-        #if the removed node is tail, removed tail
-        elif node==self.tl:
-            self.tl=self.tl.prev
-            if self.tl:
-                self.tl.next=None
-        #if the removed node is in between some nodes
-        else:
-            node.prev.next=node.next
-            node.next.prev=node.prev
-        #delete key:node from hashtable
-        del self.hashTable[node.key]   
-        
-    #add node to the head of the DLL
-    def addFirst(self,node):
-        #if the DLL is empty, the head and tail are both None, set head and tail (only used when initializing the DLL)
-        if not self.hd:
-            self.hd=node
-            self.tl=node
-        else:
-        #otherwise, set the node as head
-            node.next=self.hd
-            node.prev=None
-            self.hd.prev=node
-            self.hd=node
-        #add the <key:node> pair of the node to the hashtable
-        self.hashTable[node.key]=node
-        
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        if key not in self.hashTable:
-            return -1
-        #save node data since we will delete the node in removeNode() function.
-        tmp=Node(self.hashTable[key].key,self.hashTable[key].value)
-        #delete node
-        self.removeNode(self.hashTable[key])
-        #add the node to head
-        self.addFirst(tmp)
-        #return the node value
-        return self.hashTable[key].value
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
 
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: void
-        """
-        #if the key is in hashtable, update the node value and move the node to the head of the DLL
-        if key in self.hashTable:
-            #save node data since we will delete the node in removeNode() function.
-            tmp=Node(self.hashTable[key].key,value)
-            #delete node
-            self.removeNode(self.hashTable[key])
-            #add node to head of DLL
-            self.addFirst(tmp)
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def isEmpty(self):
+        if not self.head:
+            return True
+
+    def addNode(self, node):
+        # add to front of the list
+        if self.isEmpty():
+            self.tail = node
         else:
-            #if the key is not in the hashtable
-            newNode=Node(key,value)
-            #when the capacity is zero, no more space for new node
-            if self.capacity==0:
-                #remove tail node from DLL
-                self.removeNode(self.tl)
-                #since we remove a node, the capacity will restore space for one node
-                self.capacity+=1
-            #add new node to the DLL head
-            self.addFirst(newNode)
-            #since we add a node, the capacity will be less
-            self.capacity-=1
+            node.next = self.head
+            self.head.prev = node
+        self.head = node
+
+    def removeNode(self, node):
+        # search for the item and remove it from the list
+        if self.isEmpty():
+            return None
+        if node == self.head:
+            self.head = node.next
+            if self.head:
+                self.head.prev = None
+        elif node == self.tail:
+            self.tail = node.prev
+            if self.tail:
+                self.tail.next = None
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        # store the node with each key
+        self.cache = {}
+        # head is most recently used item, tail is least recently used
+        self.usage = DoubleLinkedList()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        val = self.cache[key].val
+        self.usage.removeNode(self.cache[key])
+
+        newNode = Node(key, val)
+        self.usage.addNode(newNode)
+        self.cache[key] = newNode
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.usage.removeNode(self.cache[key])
+            updatedNode = Node(key, value)
+            self.usage.addNode(updatedNode)
+            self.cache[key] = updatedNode
+        else:
+            newNode = Node(key, value)
+            if self.capacity == 0:
+                toEvict = self.usage.tail
+                self.usage.removeNode(toEvict)
+                self.cache.pop(toEvict.key)
+                self.capacity += 1
+
+            self.usage.addNode(newNode)
+            self.cache[key] = newNode
+            self.capacity -= 1
+
 
 
 # Your LRUCache object will be instantiated and called as such:
