f2014ad474736c2376ac4faf61320003e161a336
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2021-06-07 13:58:00 +0530

    Alt solutions for 010

--- 010_regExMatching.py
+++ 010_regExMatching.py
@@ -1,7 +1,8 @@
 class Solution(object):
+    # dp - top down
     def isMatch(self, text, pattern):
         memo = {}
-        
+
         def dp(i, j):
             if (i, j) not in memo:
                 if j == len(pattern):
@@ -17,3 +18,31 @@ class Solution(object):
             return memo[i, j]
 
         return dp(0, 0)
+
+
+    # dp - bottom up
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+
+        dp[-1][-1] = True
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                first_match = i < len(s) and p[j] in {s[i], '.'}
+                if j+1 < len(p) and p[j+1] == '*':
+                    dp[i][j] = dp[i][j+2] or first_match and dp[i+1][j]
+                else:
+                    dp[i][j] = first_match and dp[i+1][j+1]
+
+        return dp[0][0]
+
+
+    # recursion
+    # def isMatch(self, s: str, p: str) -> bool:
+    #     if not p:
+    #         return not s
+
+    #     first_match = s and p[0] in {s[0], '.'}
+    #     if len(p) >= 2 and p[1] == '*':
+    #         return self.isMatch(s, p[2:]) or (first_match and self.isMatch(s[1:], p))
+    #     else:
+    #         return first_match and self.isMatch(s[1:], p[1:])
